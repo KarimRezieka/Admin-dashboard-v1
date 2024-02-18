a60415a63c6ff61463154a9482b424251e27bfcd
KarimRezieka <karimsaadrezieka@outlook.com>
2024-02-18 20:04:50 +0200

    Teams

--- src/components/Pages/Team/Team.jsx
+++ src/components/Pages/Team/Team.jsx
@@ -1,7 +1,98 @@
+import { DataGrid } from '@mui/x-data-grid';
+import {rows} from './data.jsx';
+import { useTheme } from '@emotion/react';
+import { Box, Typography } from "@mui/material";
+import {
+  AdminPanelSettingsOutlined,
+  LockOpenOutlined,
+  SecurityOutlined,
+} from "@mui/icons-material";
+
 export default function Team(){
+  const theme = useTheme()
+   const columns = [
+    {
+      field: "id",
+      headerName: "ID",
+      width: 33,
+      align: "center",
+      headerAlign: "center",
+    },
+    {
+      field: "name",
+      headerName: "Name",
+      align: "center",
+      headerAlign: "center",
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      flex: 1,
+      align: "center",
+      headerAlign: "center",
+    },
+    { field: "age", headerName: "Age", align: "center", headerAlign: "center" },
+    {
+      field: "phone",
+      headerName: "Phone",
+      flex: 1,
+      align: "center",
+      headerAlign: "center",
+    },
+    {
+      field: "access",
+      headerName: "Access",
+      flex: 1,
+      align: "center",
+      headerAlign: "center",
+      renderCell: ({ row: { access } }) => {
+        return (
+          <Box
+            sx={{
+              p: "5px",
+              width: "99px",
+              borderRadius: "3px",
+              textAlign: "center",
+              display: "flex",
+              justifyContent: "space-evenly",
+
+              backgroundColor:
+                access === "Admin"
+                  ? theme.palette.primary.dark
+                  : access === "Manager"
+                  ? theme.palette.secondary.dark
+                  : "#3da58a",
+            }}
+          >
+            {access === "Admin" && (
+              <AdminPanelSettingsOutlined
+                sx={{ color: "#fff" }}
+                fontSize="small"
+              />
+            )}
+
+            {access === "Manager" && (
+              <SecurityOutlined sx={{ color: "#fff" }} fontSize="small" />
+            )}
+
+            {access === "User" && (
+              <LockOpenOutlined sx={{ color: "#fff" }} fontSize="small" />
+            )}
+
+            <Typography sx={{ fontSize: "13px", color: "#fff" }}>
+              {access}
+            </Typography>
+          </Box>
+        );
+      },
+    },
+  ];
   return (
-    <div >
+    <div className="team" >
       <h2>Team Page</h2>
+      <Box style={{ height: 600, width: '98%',mx:"auto" }}>
+      <DataGrid rows={rows} columns={columns} />
+    </Box>
     </div>
   );
 }
