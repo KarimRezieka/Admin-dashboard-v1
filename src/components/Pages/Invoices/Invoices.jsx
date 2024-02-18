5c2c79f92a7e661794afedb909e9e73611f43903
KarimRezieka <karimsaadrezieka@outlook.com>
2024-02-18 22:05:17 +0200

    Invoices

--- src/components/Pages/Invoices/Invoices.jsx
+++ src/components/Pages/Invoices/Invoices.jsx
@@ -1,3 +1,19 @@
+import { DataGrid } from "@mui/x-data-grid";
+import { Box } from "@mui/material";
+import { columns, rows } from "./data";
 export default function Invoices() {
-  return <div>Invoices</div>;
+  return (
+    <div>
+      <Box>
+        <Box sx={{ height: 650, mx: "auto" }}>
+          <DataGrid
+            checkboxSelection
+            rows={rows}
+            // @ts-ignore
+            columns={columns}
+          />
+        </Box>
+      </Box>
+    </div>
+  );
 }
