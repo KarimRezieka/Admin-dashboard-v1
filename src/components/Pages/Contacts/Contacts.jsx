d32fe8540208f9df59f1da14ddc3bbb4b12b875e
KarimRezieka <karimsaadrezieka@outlook.com>
2024-02-18 21:55:29 +0200

    Contacts

--- src/components/Pages/Contacts/Contacts.jsx
+++ src/components/Pages/Contacts/Contacts.jsx
@@ -1,6 +1,24 @@
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import { Box } from "@mui/material";
+import { columns, rows } from "./data";
 
-export default function Contacts() {
+
+const Contacts = () => {
   return (
-    <div className="contacts">Contacts</div>
-  )
-}
+    <Box>
+         <h2>Contacts</h2>
+      <Box sx={{ height: 650, width: "99%", mx: "auto" }}>
+        <DataGrid
+          slots={{
+            toolbar: GridToolbar,
+          }}
+          rows={rows}
+          // @ts-ignore
+          columns={columns}
+        />
+      </Box>
+    </Box>
+  );
+};
+
+export default Contacts;
