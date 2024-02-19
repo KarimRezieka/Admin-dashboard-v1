b85004de6682b6ad99ba8f96f638aa907446db3f
KarimRezieka <karimsaadrezieka@outlook.com>
2024-02-19 17:14:16 +0200

    BarChart

--- src/components/Pages/Faq/Faq.jsx
+++ src/components/Pages/Faq/Faq.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import Accordion from "@mui/material/Accordion";
 import AccordionActions from "@mui/material/AccordionActions";
 import AccordionSummary from "@mui/material/AccordionSummary";
